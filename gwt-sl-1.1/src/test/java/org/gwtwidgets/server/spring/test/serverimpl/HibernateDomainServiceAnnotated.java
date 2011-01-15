19cf81009f75e5566f15b063cf424f235962c46c
g.georgovassilis <g.georgovassilis@5132de34-37f9-2f7f-bc62-1026ee73a3d4>
2011-01-15 13:43:14 +0000

    moving 1.1 branch as 1.2 to trunk

--- gwt-sl-1.1/src/test/java/org/gwtwidgets/server/spring/test/serverimpl/HibernateDomainServiceAnnotated.java
+++
@@ -1,5 +0,0 @@
-package org.gwtwidgets.server.spring.test.serverimpl;
-
-public class HibernateDomainServiceAnnotated extends HibernateDomainServiceImpl implements AnnotatedHibernateDomainService{
-
-}
